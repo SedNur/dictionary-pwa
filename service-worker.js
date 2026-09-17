df761c57e97acdbf1318fe0e486aef7944ec1b7a
Sed Nour <30774472+SedNur@users.noreply.github.com>
2026-09-17 05:21:24 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "sntg-pwa-v4";
 
 const FILES_TO_CACHE = [
-    "./SNG_14050621.html",
+    "./SNG_14050626.html",
     "./manifest.json",
     "./ArrowClock_48_PNG.ico",
     "./icon-192.png",
